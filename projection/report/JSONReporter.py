2f1c7e29a3cc4c910cbd96c9f710e27e972843f7
raadted <a.ahmadi.k.79@gmail.com>
2023-08-26 23:01:24 +0330

    add json reporter

---
+++ projection/report/JSONReporter.py
@@ -0,0 +1,30 @@
+from colorama import Fore, Style
+from utils import rial_to_hemat, format_three_digit
+
+
+class JSONReporter:
+    def __init__(self) -> None:
+        self.memory = {}
+
+    def add_report(
+        self,
+        bazneshasteh_payment_obligation,
+        bazneshasteh_population,
+        people_income,
+        sandogh_income,
+        bimehPardaz_population,
+        sandogh_inbalance,
+        year,
+    ):
+        self.memory[year] = {
+            "bazneshasteh": {
+                "payment_obligation": rial_to_hemat(bazneshasteh_payment_obligation),
+                "alive_population": bazneshasteh_population,
+            },
+            "bimehpardaz": {
+                "people_income": rial_to_hemat(people_income),
+                "sandogh_income": rial_to_hemat(sandogh_income),
+                "alive_population": bimehPardaz_population,
+            },
+            "inbalance": {"value": sandogh_inbalance},
+        }
